eda7b2646e31a17e0bd2644e9967d08f08549d48
Alex Castro <alex.monteiro@outlook.pt>
2018-07-05 09:28:10 +0200

    reversing previous

--- lesson-3-integration-testing/server/database/seeders/20171030012943-create-recipes.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  up: queryInterface => queryInterface.bulkInsert('Recipe', [{
-    title: 'Vegetable Soup',
-    description: 'This soup is a very popular Yoruba dish.'
-  }], {}),
-
-  down: queryInterface => queryInterface.bulkDelete('Recipe', null, {})
-};
--- lesson-3-integration-testing/server/routes/index.js
+++
@@ -1,9 +0,0 @@
-import userRoutes from './users';
-import recipesRoutes from './recipes';
-import frontendRouter from './frontend';
-
-export default {
-  userRoutes,
-  recipesRoutes,
-  frontendRouter
-};
